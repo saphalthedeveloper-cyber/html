476bb6d7b0d27cd92dd25b1d23aab8908e393847
saphalthedeveloper-cyber <saphalthedeveloper@gmail.com>
2025-11-28 15:09:10 +0545

    Update setUp.js

--- setUp.js
+++ setUp.js
@@ -1,12 +1,36 @@
- function setUpFunction(){
+document.addEventListener('DOMContentLoaded',function(){
+function setUpFunction(){
     var buttons=document.getElementsByClassName("buttonn");
-    for(let i=0;i<buttons.length;i++){
-    buttons[i].onclick =function(){
-        localStorage.setItem('Selected Guitar',i);
-    window.location.href='shipping.html';
+     for(let i=0;i<buttons.length;i++){
+      buttons[i].onclick =function(){
+       
+            const parentDiv=buttons[i].closest('div');
+            const guitarImg = parentDiv.querySelector('.guitar-img1');
+            const guitarId = guitarImg.dataset.target;
+                localStorage.setItem('Selected Guitar',guitarId);
+                window.location.href='shipping.html?guitar='+guitarId;
         }
     }   
 }
-    window.onload=setUpFunction;
-
+    function setUpImg(){
+          const selectedGuitarId=localStorage.getItem('Selected Guitar');
+          const imgs = document.querySelectorAll('.guitar-img');
+          imgs.forEach(function(imgg){
+          if(imgg.dataset.target==selectedGuitarId){
+               imgg.style.display='block';
+              }
+        else{
+            imgg.style.display='none';
+            }
+        })
+       
+    }
+        if(document.querySelector('.buttonn')) {
+        setUpFunction();
+    }
+    if(document.querySelector('.guitar-img') && localStorage.getItem('Selected Guitar')) {
+        setUpImg(); 
+        }
+    });
+ 
 
